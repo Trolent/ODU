fbe3e8967edc0fb597c8a31c5ca61349fcf7af43
trolland <trolland@student.42.fr>
2024-04-07 19:30:21 +0200

    Auto-updating from 42

--- onyxia_init.sh
+++ onyxia_init.sh
@@ -7,6 +7,10 @@ sudo apt install zsh -y
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
 
-chsh -s /bin/zsh
+echo zsh >> ~/.bashrc
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+sed -i 's/^plugins=(git)$/plugins=(git zsh-syntax-highlighting)/g' ~/.zshrc`
 
 mc mirror s3/rolland ~/
